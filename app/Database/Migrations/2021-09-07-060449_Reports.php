29d2d108b06bb8cf01c711a23c57ce949608583c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-09-07 13:16:03 +0700

    create reports

---
+++ app/Database/Migrations/2021-09-07-060449_Reports.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Reports extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'cash'       => [
+                'type'       => 'INT',
+            ],
+            'datetime'       => [
+                'type'       => 'timestamp',
+            ],
+            'detail'       => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'debit'       => [
+                'type'       => 'INT',
+                'null' => true,
+            ],
+            'credit'       => [
+                'type'       => 'INT',
+                'null' => true,
+            ],
+            'balance'       => [
+                'type'       => 'INT',
+            ],
+            'created_date datetime default current_timestamp',
+            'updated_date datetime default current_timestamp on update current_timestamp',
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('reports');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('reports');
+    }
+}
---
+++ app/Database/Seeds/Reports.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Reports extends Seeder
+{
+    public function run()
+    {
+        //
+    }
+}
